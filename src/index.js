b0b749490479bf736f396c34f169a9c9e3ada882
Zakaria <zakariaelbarmaki03@gmail.com>
2026-09-21 10:53:15 +0200

    basic API call

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "./styles.css";
+
+// const location = "krakow";
+
+const form = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+
+const content = document.getElementById("content");
+
+async function getWeatherData(location) {
+  content.innerHTML = "Loading...";
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${process.env.WEATHER_API}`,
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const weatherData = await response.json();
+
+    console.log(weatherData);
+    content.innerHTML = `${weatherData.address}`;
+  } catch {
+    content.innerHTML = `
+    <div>Unable to find city: ${location}</div>
+    `;
+  }
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  getWeatherData(searchInput.value);
+});
--- webpack.common.js
+++ webpack.common.js
@@ -1,39 +1,40 @@
 import path from "node:path";
-import { fileURLToPath } from 'node:url';
+import { fileURLToPath } from "node:url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-
+import Dotenv from "dotenv-webpack";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-    entry: {
-        app: "./src/index.js",
-    },
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-        template: "./src/index.html",
-        }),
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new Dotenv(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: "asset/resource",
+      },
     ],
-    module: {
-        rules: [
-        {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        },
-        {
-            test: /\.html$/i,
-            use: ["html-loader"],
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-            type: "asset/resource",
-        },
-        ],
-    },
+  },
 };
